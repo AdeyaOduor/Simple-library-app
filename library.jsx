f2a845209f9723bb4147b76633aab11ed0dac417
DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2020-12-28 01:07:18 +0300

    Add files via upload

---
+++ library.jsx
@@ -0,0 +1,66 @@
+class LibraryApp extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { items: [], text: '' };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    render() {
+      return (
+        <div>
+          <h3>My Library</h3>
+          <LibraryApp items={this.state.items} />
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="new-todo">
+              Add a book!
+            </label>
+            <input
+              id="new-title"
+              onChange={this.handleChange}
+              value={this.state.text}
+            />
+            <button>
+              Add #{this.state.items.length + 1}
+            </button>
+          </form>
+        </div>
+      );
+    }
+  
+    handleChange(e) {
+      this.setState({ text: e.target.value });
+    }
+  
+    handleSubmit(e) {
+      e.preventDefault();
+      if (this.state.text.length === 0) {
+        return;
+      }
+      const newItem = {
+        text: this.state.text,
+        id: Date.now()
+      };
+      this.setState(state => ({
+        items: state.items.concat(newItem),
+        text: ''
+      }));
+    }
+  }
+  
+  class LibraryApp extends React.Component {
+    render() {
+      return (
+        <ul>
+          {this.props.items.map(item => (
+            <li key={item.id}>{item.text}</li>
+          ))}
+        </ul>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <LibraryApp />,
+    document.getElementById('new-title')
+  );
